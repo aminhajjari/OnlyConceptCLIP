4f673dc009e61f4708b403631bd1d25b7a3c741e
Amin Hajjari <107117597+aminhajjari@users.noreply.github.com>
2025-09-09 22:09:39 +0330

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -33,7 +33,7 @@ from ConceptModel.preprocessor_conceptclip import ConceptCLIPProcessor
 # Your dataset paths (Narval specific)
 DATASET_PATH = "/project/def-arashmoh/shahab33/XAI/MILK10k_Training_Input/MILK10k_Training_Input"
 GROUNDTRUTH_PATH = "/project/def-arashmoh/shahab33/XAI/MILK10k_Training_Input/groundtruth.csv"
-OUTPUT_PATH = "/project/def-arashmoh/shahab33/XAI/MILK10k_Training_Input/outputs"
+OUTPUT_PATH = "/project/def-arashmoh/shahab33/XAI/MILK10k_Training_Input/Conoutput"
 
 # Local model paths
 CONCEPTCLIP_MODEL_PATH = "/project/def-arashmoh/shahab33/XAI/MILK10k_Training_Input/ConceptModel"
